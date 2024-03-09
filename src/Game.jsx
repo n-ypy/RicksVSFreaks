b5ba1e21d29860addee2c42535ef72b716ba20ae
enypy <g404ebus@gmail.com>
2024-03-09 12:40:27 +0100

    img preload fn

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,12 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import HeroSelection from './Components/HeroSelection';
 import './assets/styles/Game.css';
 import Fight from './Components/Fight';
 import './assets/styles/StarBackground.css'
+import images from './libs/imagesList';
+import getImageUrl from './utils/getImageUrl';
 
 export default function Game() {
 
     const [isFightInitiated, setIsFightInitiated] = useState(false)
+
+    const preloadImages = () => {
+        images.forEach((imgPath) => {
+            const img = new Image();
+            img.src = getImageUrl(imgPath);
+        })
+    }
+
+    useEffect(() => {
+        preloadImages()
+    }, [])
 
     return (<>
         <div id='stars'></div>
